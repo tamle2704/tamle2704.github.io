ffa7072d4a501cdbc680721c10fb61ebe7f9a2c4
tamle2704 <53324999+tamle2704@users.noreply.github.com>
2019-12-04 22:26:32 +0700

    update gymzone

--- gymzone/js/main.js
+++ gymzone/js/main.js
@@ -184,3 +184,24 @@ $(document).ready(function () {
     });
 });
 
+//-----------------------------VIDEO-MODAL------------------
+$(document).ready(function () {
+    autoPlayYouTubeModal();
+});
+
+function autoPlayYouTubeModal() {
+        var trigger = $('.videoModalTriger');
+    trigger.click(function () {
+        var theModal = $(this).data("target");
+        var videoSRC = $(this).attr("data-videoModal");
+        var videoSRCauto = videoSRC + "?autoplay=1";
+        $(theModal + ' iframe').attr('src', videoSRCauto);
+        $(theModal).on('hidden.bs.modal', function (e) {
+            $(theModal + ' iframe').attr('src', '');
+        });
+    });
+};
+    
+    
+
+
